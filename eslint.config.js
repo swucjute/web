644b7ceaa86fa206eb3b8b7150eced70138384d6
dhkang0912 <dhkang960912@gmail.com>
2026-09-05 15:13:54 +0900

    feat: Orval React Query 클라이언트 구성

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'figmaMaker']),
+  globalIgnores(['dist', 'figmaMaker', 'src/api/generated', 'src/api/model']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
